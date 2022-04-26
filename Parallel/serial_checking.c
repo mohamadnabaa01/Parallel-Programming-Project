f75fe6128aed149e7205eda807265276bb6d95ff
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-04-26 03:13:49 +0300

    added check_occurrences and file

--- Parallel/serial_checking.c
+++ Parallel/serial_checking.c
@@ -25,7 +25,7 @@ int main(void) {
 
     for(int i=0; i<SIZE; i++){
         if(check_occurences[i] != 0)
-            printf("The char %s is repeated %d times in the string", (char) i + 'a', check_occurences[i]);
+            fprintf("The char %s is repeated %d times in the string", (char) i + 'a', check_occurences[i]);
     }
 
     fflush(stdout);
