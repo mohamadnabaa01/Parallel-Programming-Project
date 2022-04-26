c016faca58703b7dc4cc4643415ecaa51eea7736
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-04-26 03:31:50 +0300

    update

--- Parallel/serial_checking.c
+++ Parallel/serial_checking.c
@@ -3,33 +3,29 @@
 #include <time.h>
 #define SIZE 26
 #define LENGTH 100
-int main(void) {
+int main(void)
+{
     FILE *file;
-    file = fopen("string.txt", "r");//open the file fname
-    if(!file)//if open failed
+    file = fopen("string.txt", "r"); // open the file fname
+    if (!file)                       // if open failed
         return -1;
     int string_length = 0;
     fscanf(file, "%d", &string_length);
     printf("Length of string is: %d", string_length);
     char string[string_length];
-    fscanf(file, "%[^\n]\n", string);//read the contents of the file and put in string
-    
+    fscanf(file, "%[^\n]\n", string); // read the contents of the file and put in string
+    printf("The string is: %s", string);
+
     int check_occurences[SIZE];
-    for(int i=0;i<SIZE;i++){
-        check_occurences[i]=0;
-    }
+    for (int i = 0; i < SIZE; i++)
+        check_occurences[i] = 0;
 
-    for(int i=0; i<string_length; i++){
+    for (int i = 0; i < string_length; i++)
         check_occurences[string[i] - 'a']++;
-    }
 
-    for(int i=0; i<SIZE; i++){
-        if(check_occurences[i] != 0)
-            printf("The char %c is repeated %d times in the string", ( (char) (i + 'a') ), check_occurences[i]);
-    }
+    for (int i = 0; i < SIZE; i++)
+        if (check_occurences[i] != 0)
+            printf("The char %c is repeated %d times in the string", ((char)(i + 'a')), check_occurences[i]);
 
-    
-    printf("%s", string);
     return 0;
 }
-
