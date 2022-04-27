73d7f87c825d5fabfd31ed23d16b641e8d5c10f2
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-04-28 00:39:08 +0300

    debugging

--- Parallel/parallel_checking.c
+++ Parallel/parallel_checking.c
@@ -5,20 +5,22 @@
 #include <stddef.h>
 #include <string.h>
 
+#define TOTAL_CHARS 26
+
 typedef struct OccurrenceCheck
 {
     char character;
     int occurrence;
 } occurrence_num;
 
-occurrence_num nums[26];
+occurrence_num nums[TOTAL_CHARS];
 
 int main(int argc, char **argv)
 {
     int rank = 0, size = 0;
     int number_of_characters = 0;
     char *string;
-    for (int i = 0; i < 26; i++)
+    for (int i = 0; i < TOTAL_CHARS; i++)
     {
         nums[i].character = (char)('a' + i);
         nums[i].occurrence = 0;
@@ -67,7 +69,7 @@ int main(int argc, char **argv)
 
     if (rank == size - 1)
     {
-        for (int i = 0; i < 26; i++)
+        for (int i = 0; i <= TOTAL_CHARS; i++)
         {
             printf("The char %c is repeated %d times in the string\n", nums[i].character, nums[i].occurrence);
         }
