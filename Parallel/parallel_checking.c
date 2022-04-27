e24861336cf567de950180cae7c76e94edac8ad3
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-04-27 11:40:11 +0300

    added occurrences struct

--- Parallel/parallel_checking.c
+++ Parallel/parallel_checking.c
@@ -4,15 +4,18 @@
 #include <time.h>
 #include <stddef.h>
 
-typedef struct occurrence_t{
-    int index;
+typedef struct OccurrenceCheck{
     char character;
     int occurrence;
-} CharacterOccurrence;
+}occurrence_num;
 
+occurrence_num nums[26];
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
     int rank = 0, size = 0;
+    int number_of_characters = 0;
+    char string[number_of_characters];
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -21,19 +24,37 @@ int main(int argc, char** argv){
     MPI_Barrier(MPI_COMM_WORLD);
     start = MPI_Wtime();
 
-    FILE *file;
-    file = fopen("string.txt", "r"); // open the file fname
-    if (!file)                       // if open failed
-        return -1;
-    int number_of_characters = 0;
-    fscanf(file, "%d\n", &number_of_characters);
-    printf("Length of string is: %d\n", number_of_characters);
-    char string[number_of_characters];
-    fscanf(file, "%[^\n]\n", string); // read the contents of the file and put in string
-    printf("The string is: %s\n", string);
+    if (rank == 0)
+    {
+        FILE *file;
+        file = fopen("string.txt", "r"); // open the file fname
+        if (!file)                       // if open failed
+            return -1;
+        fscanf(file, "%d\n", &number_of_characters);
+        printf("Length of string is: %d\n", number_of_characters);
+        fscanf(file, "%[^\n]\n", string); // read the contents of the file and put in string
+        printf("The string is: %s\n", string);
+
+        for(int i = 0; i < 26; i++){
+            nums[i].character = (char) ('a' + i);
+            nums[i].occurrence = 0;
+        }
+    }
 
     int num_of_chars_per_processor = number_of_characters / size;
 
+    char* received_chars_per_processor = (char*) malloc(sizeof(char) * num_of_chars_per_processor);
 
+    MPI_Scatter(string, number_of_characters, MPI_CHAR, received_chars_per_processor, num_of_chars_per_processor, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+    for(int i = 0; i < num_of_chars_per_processor; i++){
+        int index = received_chars_per_processor[i] - 'a';
+        nums[index].occurrence++;
+    }
+
+    if(rank == size - 1){
+        for(int i = 0; i < 26; i++){
+            printf("The char %c is repeated %d times in the string", nums[i].character, nums[i].occurrence);
+        }
+    }
 }
-
