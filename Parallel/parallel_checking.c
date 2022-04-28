f7a4155979398ca9e6f045d401e552b3b351d963
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-04-28 04:10:48 +0300

    updating

--- Parallel/parallel_checking.c
+++ Parallel/parallel_checking.c
@@ -18,6 +18,11 @@ occurrence_num nums[TOTAL_CHARS];
 int main(int argc, char **argv)
 {
     int rank = 0, size = 0;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     if (rank == 0)
     {
         for (int i = 0; i < TOTAL_CHARS; i++)
@@ -25,12 +30,7 @@ int main(int argc, char **argv)
             nums[i].character = (char)('a' + i);
             nums[i].occurrence = 0;
         }
-        printf("Hi");
     }
-
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     double start, end;
     MPI_Barrier(MPI_COMM_WORLD);
