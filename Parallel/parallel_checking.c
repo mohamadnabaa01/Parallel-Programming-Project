7c261579195d318d719d74e45fb47db65fd0bec6
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-04-28 15:34:00 +0300

    debug

--- Parallel/parallel_checking.c
+++ Parallel/parallel_checking.c
@@ -6,27 +6,28 @@
 
 #define TOTAL_CHARS 26
 
-typedef struct OccurrenceCheck
-{
-    char character;
-    int occurrence;
-} occurrence_num;
+// typedef struct OccurrenceCheck
+// {
+//     char character;
+//     int occurrence;
+// } occurrence_num;
 
 int main(int argc, char **argv)
 {
     int rank = 0, size = 0;
-    occurrence_num nums[TOTAL_CHARS];
+    // occurrence_num nums[TOTAL_CHARS];
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    for (int i = 0; i < TOTAL_CHARS; i++)
-    {
-        nums[i].character = (char)('a' + i);
-        nums[i].occurrence = 0;
-    }
-
+    // for (int i = 0; i < TOTAL_CHARS; i++)
+    // {
+    //     nums[i].character = (char)('a' + i);
+    //     nums[i].occurrence = 0;
+    // }
+    int chars_occurrences[TOTAL_CHARS];
+    MPI_Barrier(MPI_COMM_WORLD);
     double start = MPI_Wtime();
 
     char *string;
@@ -41,8 +42,8 @@ int main(int argc, char **argv)
     string = (char *)malloc(sizeof(char) * number_of_characters);
     fscanf(file, "%[^\n]\n", string);
 
-    MPI_Bcast(&number_of_characters, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(string, number_of_characters, MPI_CHAR, 0, MPI_COMM_WORLD);
+    // MPI_Bcast(&number_of_characters, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    // MPI_Bcast(string, number_of_characters, MPI_CHAR, 0, MPI_COMM_WORLD);
 
     int num_of_chars_per_processor = number_of_characters / size;
 
@@ -52,14 +53,15 @@ int main(int argc, char **argv)
     for (int i = low; i < high; i++)
     {
         int index = (int)string[i] - 'a';
-        nums[index].occurrence++;
+        chars_occurrences[index]++;
+        // nums[index].occurrence++;
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
     printf("Rank %d:\n", rank);
     for (int i = 0; i <= TOTAL_CHARS; i++)
     {
-        printf("The char %c is repeated %d times in the string\n", nums[i].character, nums[i].occurrence);
+        printf("The char %c is repeated %d times in the string\n", (char) i + 'a', chars_occurrences[i]);
     }
 
     if (rank == 0)
